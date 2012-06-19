4533b9598ea4433952b6b1172a6c002831027818
Felix Geller <fgeller@gmail.com>
2012-06-19 23:18:40 +0200

    Support for multiple regular expressions per major mode.
    tabs -> spaces.

--- jumper-update.el
+++ jumper-update.el
@@ -43,59 +43,83 @@
 (defvar *jumper-update-mode-to-def-regex-list* ())
 (setq *jumper-update-mode-to-def-regex-list*
       '(
-	(python-mode . ("^[	]*\\(def\\|class\\) \\([_A-Za-z][_A-Za-z1-9]*\\)(.*" . 2))
-	))
+        (python-mode . (("^[  ]*\\(def\\|class\\) \\([_A-Za-z][_A-Za-z1-9]*\\)(.*$" . 2)))
+        (js2-mode . (("^[     ]*Ext.define(.+\\.\\([_A-Za-z][^'\"]*\\).*$" . 1)
+                     ("^[	]*\\([_A-Za-z][_A-Za-z1-9]*\\): function.*$" . 1)))
+        ))
 
 
 (defun jumper-update-log (str)
   (when *jumper-update-debug*
-      (message (replace-regexp-in-string "%" "%%" str))))
+    (message (replace-regexp-in-string "%" "%%" str))))
 
 
-(defun jumper-update-strip-buffer-to-defs (def-regex)
-  (let ((line-number 0)
-	(regex (car def-regex))
-	(replace-str (format "\\%s" (cdr def-regex))))
+(defun jumper-update-delete-line ()
+  (jumper-update-log (format "Deleting line [%s]" (jumper-update-current-line)))
+  (delete-region (point-at-bol) (point-at-eol)))
+
+
+(defun jumper-update-defify-line (replace-str file-name line-number)
+  (jumper-update-log "Replacing a found def and adding location info.")
+  (replace-match replace-str nil nil)
+  (goto-char (point-at-eol))
+  (insert (format "       %s      %s" file-name line-number)))
+
+
+(defun jumper-update-current-line ()
+  (buffer-substring (point-at-bol) (point-at-eol)))
+
+
+(defun jumper-update-defify-or-delete-line (def-regexs file-name line-number)
+  (if def-regexs
+      (let ((regex (caar def-regexs))
+            (replace-str (format "\\%s" (cdar def-regexs))))
+        (jumper-update-log (format "Looking for [%s]" regex))
+        (if (re-search-forward regex (point-at-eol) t)
+            (jumper-update-defify-line replace-str file-name line-number)
+          (jumper-update-defify-or-delete-line (cdr def-regexs) file-name line-number)))
+    (jumper-update-delete-line)))
+
+
+(defun jumper-update-strip-buffer-to-defs (def-regexs file-name)
+  (let ((line-number 0))
+    (goto-char (point-min))
     (while (not (eobp))
       (setq line-number (1+ line-number))
       (goto-char (point-at-bol))
-      (if (re-search-forward regex (point-at-eol) t)
-	  (progn
-	    (replace-match replace-str nil nil)
-	    (goto-char (point-at-eol))
-	    (insert (format "	%s	%s" file-name line-number))
-	    (forward-line))
-	(let ((kill-whole-line t))
-	  (kill-line)))
-      )))
+      (jumper-update-log (format "Looking at line [%s]" (jumper-update-current-line)))
+      (jumper-update-defify-or-delete-line def-regexs file-name line-number)
+      (forward-line 1))
+    (goto-char (point-min))
+    (flush-lines "^[        ]*$")))
 
 
 (defun jumper-update-defs-from-current-buffer ()
   (interactive)
   (let* ((file-name (file-truename (buffer-file-name)))
-	 (buffer-with-defs (current-buffer))
-	 (def-regex (cdr (assoc major-mode *jumper-update-mode-to-def-regex-list*)))
-	 (jumper-file (jumper-find-jumper-file)))
-    (when (and def-regex jumper-file)
+         (buffer-with-defs (current-buffer))
+         (def-regexs (cdr (assoc major-mode *jumper-update-mode-to-def-regex-list*)))
+         (jumper-file (jumper-find-jumper-file)))
+    (when (and def-regexs jumper-file)
       (with-temp-buffer
-	(insert-buffer-substring buffer-with-defs)
-	(goto-char (point-min))
-	(jumper-update-strip-buffer-to-defs def-regex)
-	(jumper-update-log (format "Found defs: \n%s" (buffer-substring (point-min) (point-max))))
-	(jumper-update-defs-in-jumper-file
-	 jumper-file
-	 file-name
-	 (buffer-substring (point-min) (point-max)))
-	))))
+        (insert-buffer-substring buffer-with-defs)
+        (goto-char (point-min))
+        (jumper-update-strip-buffer-to-defs def-regexs file-name)
+        (jumper-update-log (format "Found defs: \n[%s]" (buffer-substring (point-min) (point-max))))
+        (jumper-update-defs-in-jumper-file
+         jumper-file
+         file-name
+         (buffer-substring (point-min) (point-max)))
+        ))))
 
 
 (defun jumper-update-defs-in-jumper-file (jumper-file file-name defs)
-    (set-buffer (find-file-noselect jumper-file))
-    (goto-char (point-min))
-    (flush-lines file-name)
-    (goto-char (point-max))
-    (insert defs)
-    (save-buffer))
+  (set-buffer (find-file-noselect jumper-file))
+  (goto-char (point-min))
+  (flush-lines file-name)
+  (goto-char (point-max))
+  (insert defs)
+  (save-buffer))
 
 
 (provide 'jumper-update)
