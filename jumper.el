3e90b2af7ddfd7f4e8a77c256b154ffdc951b553
Peter Seibel <peter@gigamonkeys.com>
2017-05-02 20:35:54 -0700

    Slight tweak to Python stack trace pattern.

--- jumper.el
+++ jumper.el
@@ -63,7 +63,7 @@
     "from \\([^:]+\\):\\([[:digit:]]+\\)"                                    ; Ruby stack trace
     "at \\([^ ]+\\) line \\([[:digit:]]+\\)[,.]"                             ; Perl errors
     "in \\([^ ]+\\) on line \\([[:digit:]]+\\)"                              ; PHP errors
-    "File \"\\([^\"]+\\)\", line \\([[:digit:]]+\\),"                        ; Python stack trace
+    "File \"\\([^\"]+\\)\", line \\([[:digit:]]+\\)"                        ; Python stack trace
     "#[[:blank:]]+\\(?:\\(?:modified\\|new file\\):[[:blank:]]+\\)?\\(.+\\)" ; git status
     "\\(.+\\)"
     "[[:blank:]]*\\([^ ]+\\)"
